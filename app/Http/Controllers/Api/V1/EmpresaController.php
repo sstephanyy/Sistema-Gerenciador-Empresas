4157270fb5a149d1509a30666f83a98be7fc0d55
Stephanyy <santosstephany400@gmail.com>
2024-03-20 07:35:52 -0300

    removing comments

--- app/Http/Controllers/Api/V1/EmpresaController.php
+++ app/Http/Controllers/Api/V1/EmpresaController.php
@@ -16,35 +16,24 @@ class EmpresaController extends Controller
         return EmpresaResource::collection(Empresas::all());
     }
 
-    /**
-     * Store a newly created resource in storage -- POST.
-     */
     public function store(GuardarEmpresaRequest $request)
     {
         Empresas::create($request->validated());
         return response()->json("Empresa adicionada ao bandco de dados com sucesso.");
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Empresas $empresa)
     {
         return new EmpresaResource($empresa);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(GuardarEmpresaRequest $request, Empresas $empresa)
     {
         $empresa->update($request->validated());
         return response()->json("Empresa atualizada");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Empresas $empresa)
     {
         $empresa->delete();
